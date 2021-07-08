1035f0b437d491f2f780901fc4b94919de0ffc40
holmofy <hff1996723@163.com>
2021-07-08 14:16:09 +0800

    all in one starter

---
+++ starter/src/main/java/com/doutiaotech/apollo/ApolloMain.java
@@ -0,0 +1,13 @@
+package com.doutiaotech.apollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloMain.class, args);
+    }
+
+}
