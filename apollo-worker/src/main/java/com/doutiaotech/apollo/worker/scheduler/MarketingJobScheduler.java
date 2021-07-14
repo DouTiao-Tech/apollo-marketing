4c8b444de232f7c7760e0a00fcebed683dbf3b18
holmofy <hff1996723@163.com>
2021-07-14 13:28:16 +0800

    log config

--- apollo-worker/src/main/java/com/doutiaotech/apollo/worker/scheduler/MarketingJobScheduler.java
+++ apollo-worker/src/main/java/com/doutiaotech/apollo/worker/scheduler/MarketingJobScheduler.java
@@ -1,6 +1,8 @@
 package com.doutiaotech.apollo.worker.scheduler;
 
+import com.doutiaotech.apollo.infrastructure.clickhouse.mapper.TradeMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +10,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class MarketingJobScheduler {
 
+    @Autowired
+    private TradeMapper tradeMapper;
+
     @Scheduled(fixedDelay = 1000)
     public void schedule() {
         log.info("do marketing job");
+        tradeMapper.findById(1L);
     }
 
 }
--- infrastructure-clickhouse/src/main/java/com/doutiaotech/apollo/infrastructure/clickhouse/mapper/TradeMapper.java
+++ infrastructure-clickhouse/src/main/java/com/doutiaotech/apollo/infrastructure/clickhouse/mapper/TradeMapper.java
@@ -5,5 +5,5 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TradeMapper {
-    Trade findById(Long id);
+    Trade findById(Long orderId);
 }
