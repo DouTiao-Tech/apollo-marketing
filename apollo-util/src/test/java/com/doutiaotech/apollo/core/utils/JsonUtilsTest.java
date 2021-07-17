7ca77d23bcdfd3727d1f143c1bd8152843a92c26
holmofy <hff1996723@163.com>
2021-07-17 15:33:50 +0800

    fix junit and add hamcrest assert

--- apollo-util/src/test/java/com/doutiaotech/apollo/core/utils/JsonUtilsTest.java
+++ apollo-util/src/test/java/com/doutiaotech/apollo/core/utils/JsonUtilsTest.java
@@ -1,38 +1,45 @@
 package com.doutiaotech.apollo.core.utils;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+
+import org.junit.Test;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.junit.Assert;
-import org.junit.Test;
 
 public class JsonUtilsTest {
 
     @Test
     public void test_toJson() {
         String json = JsonUtils.toJson(new SimpleObject("value"));
-        Assert.assertEquals("{\"key\":\"value\"}", json);
+        assertThat(json, equalTo("{\"key\":\"value\"}"));
         json = JsonUtils.toJson(new SnakeObject("value"));
-        Assert.assertEquals("{\"snake_key\":\"value\"}", json);
+        assertThat(json, equalTo("{\"snake_key\":\"value\"}"));
         json = JsonUtils.toJson(new CamelObject("value"));
-        Assert.assertEquals("{\"camelKey\":\"value\"}", json);
-        Assert.assertThrows(NullPointerException.class, () -> JsonUtils.toJson(null));
+        assertThat(json, equalTo("{\"camelKey\":\"value\"}"));
+        assertThrows(NullPointerException.class, () -> JsonUtils.toJson(null));
     }
 
     @Test
     public void test_fromJson() {
         SimpleObject simpleObject = JsonUtils.fromJson("{\"key\":\"value\"}", SimpleObject.class);
-        Assert.assertEquals("value", simpleObject.key);
+        assertEquals("value", simpleObject.key);
         SnakeObject snake_object = JsonUtils.fromJson("{\"snake_key\":\"value\"}", SnakeObject.class);
-        Assert.assertEquals("value", snake_object.snake_key);
+        assertEquals("value", snake_object.snake_key);
         CamelObject camelObject = JsonUtils.fromJson("{\"camelKey\":\"value\"}", CamelObject.class);
-        Assert.assertEquals("value", camelObject.camelKey);
-        Assert.assertThrows(NullPointerException.class, () -> JsonUtils.fromJson(null, SimpleObject.class));
-        Assert.assertThrows(MismatchedInputException.class, () -> JsonUtils.fromJson("", SimpleObject.class));
-        Assert.assertThrows(IllegalArgumentException.class, () -> JsonUtils.fromJson("{\"key\":\"value\"}", null));
-        Assert.assertThrows(UnrecognizedPropertyException.class, () -> JsonUtils.fromJson("{\"key\":\"value\"}", SnakeObject.class));
+        assertEquals("value", camelObject.camelKey);
+        assertThrows(NullPointerException.class, () -> JsonUtils.fromJson(null, SimpleObject.class));
+        assertThrows(MismatchedInputException.class, () -> JsonUtils.fromJson("", SimpleObject.class));
+        assertThrows(IllegalArgumentException.class, () -> JsonUtils.fromJson("{\"key\":\"value\"}", null));
+        assertThrows(UnrecognizedPropertyException.class,
+                () -> JsonUtils.fromJson("{\"key\":\"value\"}", SnakeObject.class));
     }
 
     @Data
