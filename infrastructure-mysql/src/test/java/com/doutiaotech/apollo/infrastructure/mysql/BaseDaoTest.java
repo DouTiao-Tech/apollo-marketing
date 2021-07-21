7b8f66ba2ef7e11c17fabc5ebda672c76eca22e6
holmofy <hff1996723@163.com>
2021-07-21 14:53:11 +0800

    move properties

--- infrastructure-mysql/src/test/java/com/doutiaotech/apollo/infrastructure/mysql/BaseDaoTest.java
+++ infrastructure-mysql/src/test/java/com/doutiaotech/apollo/infrastructure/mysql/BaseDaoTest.java
@@ -1,10 +1,6 @@
 package com.doutiaotech.apollo.infrastructure.mysql;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import com.doutiaotech.apollo.infrastructure.mysql.BaseDaoTest.TestConfig;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +8,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {TestConfig.class}, properties = {"embedded.containers.forceShutdown=true",
-        "embedded.mysql.dockerImage=mysql:5.6.50", "embedded.containers.reuseContainer=true",
-        "embedded.mysql.init-script-path=schema.sql",
-        "mysql.datasource.jdbcUrl=jdbc:mysql://${embedded.mysql.host}:${embedded.mysql.port}/${embedded.mysql.schema}?useSSL=false",
-        "mysql.datasource.driverClassName=com.mysql.cj.jdbc.Driver", "mysql.datasource.username=${embedded.mysql.user}",
-        "mysql.datasource.password=${embedded.mysql.password}", "spring.main.banner-mode=off", "logging.level.root=INFO"})
+@SpringBootTest(classes = {TestConfig.class})
+@TestPropertySource("classpath:application-unit-test.properties")
 @Transactional
 public abstract class BaseDaoTest {
 
